b516d2b2965ef145ff0ae11c3479b51f1cce3944
Ivan_Baryshev <obivanivan@mail.ru>
2023-11-24 14:39:04 +0300

    upd

--- test/BookServiceTest.java
+++ test/BookServiceTest.java
@@ -1,3 +1,7 @@
+/*Задание 2.
+
+У вас есть класс BookService, который использует интерфейс BookRepository для получения информации о книгах из базы данных. Ваша задача написать unit-тесты для BookService, используя Mockito для создания мок-объекта BookRepository. */
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
